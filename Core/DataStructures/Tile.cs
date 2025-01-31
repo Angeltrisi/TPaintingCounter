1d18071d797d5d7478cb9dd5f0046011c5cb91cc
queue <angeltrisix@gmail.com>
2025-01-30 21:12:23 -0500

    cleanup

--- Core/DataStructures/Tile.cs
+++ Core/DataStructures/Tile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TPaintingCounter.Core.DataStructures
+﻿namespace TPaintingCounter.Core.DataStructures
 {
     public struct Tile()
     {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TPaintingCounter.Core;
+﻿using TPaintingCounter.Core;
 
 static void Start()
 {
